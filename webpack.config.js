e18ae83036e098e03aaefe1383ade129e6c08d98
ykang2020 <yk1123@vip.163.com>
2021-04-18 16:38:41 +0800

    初始化仓库

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-  // 模式
-  mode: 'development', // 也可以使用 production
   // 入口
-  entry: './src/index.js', 
+  entry: "./src/index.js",
   // 出口
   output: {
     // 打包文件夹
-    path: path.resolve(__dirname, 'dist'),
+    publicPath: "dist",
     // 打包文件
-    filename: 'yk-utils.js', 
-    // 向外暴露的对象的名称
-    library: 'ykUtils',
-    // 打包生成库可以通过esm/commonjs/reqirejs的语法引入
-    libraryTarget: 'umd', 
+    filename: "yk-utils.js",
   },
-}
+  // 配置webpack-dev-server
+  devServer: {
+    // 静态根目录
+    contentBase: "www",
+    // 端口号
+    port: 8080,
+  },
+};
