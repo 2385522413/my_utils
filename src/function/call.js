af635074d78b1f8d9af38d2342b870efd8ab356c
ykang2020 <yk1123@vip.163.com>
2021-04-18 17:47:16 +0800

    上传课程代码

---
+++ src/function/call.js
@@ -0,0 +1,14 @@
+function call(Fn, obj, ...args) {
+  if (obj === undefined || obj === null) {
+    // 表示全局对象（ES11新增特性）
+    obj = globalThis;
+  }
+  // 为 obj 添加临时的方法
+  obj.temp = Fn;
+  // 调用 temp 方法
+  let result = obj.temp(...args);
+  // 删除tempfangfa
+  delete obj.temp;
+  // 返回执行结果
+  return result;
+}
