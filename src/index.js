d324121ae296256a44d0e7f7336e5007acd81f84
ykang2020 <yk1123@vip.163.com>
2021-04-21 18:15:28 +0800

    完成函数节流

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-import call from "./function/call";
-import apply from "./function/apply";
-import bind from "./function/bind";
+import throttle from "./function/throttle.js";
 
-console.log("****test call ****");
+// window.addEventListener("scroll", function () {
+//   console.log(Date.now);
+// });
 
-function add(a, b) {
-  console.log(this);
-  return a + b + this.c;
-}
-let obj = {
-  c: 521,
-};
-window.c = 1314;
-
-console.log(call(add, obj, 10, 20))
-console.log(call(add, null, 30, 40))
-
-console.log(obj)
-console.log("****test apply****");
-console.log(apply(add, obj, [10, 20]))
-console.log(apply(add, null, [30, 40]))
-
-console.log(obj)
-console.log("****test bind****");
-let fn = bind(add, obj, 10, 20);
-console.log(fn());
-
-let fn2 = bind(add, obj);
-console.log(fn2(10, 20));
+window.addEventListener(
+  "scroll",
+  throttle(function (e) {
+    console.log(e);
+  }, 500)
+);
